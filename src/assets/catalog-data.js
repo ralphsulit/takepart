c920c8de549b98c63bde6d984bc6da41f76a61c4
Joseph <ralphlsulit@gmail.com>
2022-09-09 18:40:15 +0800

    added catalog-data files in assets

---
+++ src/assets/catalog-data.js
@@ -0,0 +1,26 @@
+const catalogData = [
+  {
+    id: 1,
+    title: 'Fibre Cable Tight Buffered MM 16 Fibre 0m3 50/125 LSZH Eca Black (Dia)7.8mm',
+    description: 'Distribution or mini-break-out cable suitable for use in many indoor and outdoor applications. Features new and improved ES9 tight buffer. Typical applications include LAN and WAN backbones...',
+    number: 60019533
+  },
+  {
+    id: 2,
+    title: 'DYNAmic Server Cabinet 47Ux6x10 Vented Vented Removable Adj Feet & Earh Kit',
+    description: 'This standard series of Lande cabinets provides an ideal environment for housing server and IT equipment, available from 26U to 47U and up to 1200mm deep',
+    number: 'DAT60019533'
+  },
+  {
+    id: 1,
+    title: 'Fibre Cable Tight Buffered MM 16 Fibre 0m3 50/125 LSZH Eca Black (Dia)7.8mm',
+    description: 'The metal content cable tie is designed for use in the food and pharmaceutical processing industries. A unique manufacturing process, involving the inclusion of a metallic pigment',
+    number: 60019533
+  },
+  {
+    id: 1,
+    title: 'Fibre Cable Tight Buffered MM 16 Fibre 0m3 50/125 LSZH Eca Black (Dia)7.8mm',
+    description: 'Fluke Networks DSX2-5000 copper cabling certifier. Tests up to TIA Cat6A or ISO/IEC Class FA. Full Cat5e/6 2-way autotest in just 10 seconds. WiFi enabled.',
+    number: '60019533LAO1'
+  },
+];
