05cda9703cc9aa546a3c83ac142693b71f147525
Joseph <ralphlsulit@gmail.com>
2022-09-11 05:43:41 +0800

    added bootstrap navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import CatalogMenu from './components/catalog-menu/catalog-menu.component.jsx';
+import Navbar from './routes/navigation.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
+      <Navbar>This is a button</Navbar>
       <CatalogMenu />
     </div>
   );
