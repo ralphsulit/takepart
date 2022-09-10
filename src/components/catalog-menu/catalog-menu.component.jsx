576bae9de82bcb6fdb173029fd02fcf3fc1cb155
Joseph <ralphlsulit@gmail.com>
2022-09-11 04:51:35 +0800

    change some styles in mobile view

--- src/components/catalog-menu/catalog-menu.component.jsx
+++ src/components/catalog-menu/catalog-menu.component.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import SearchBox from '../search-box/search-box.component.jsx';
 import catalogData from '../../assets/data/catalog-data.json';
 import CatalogItem from '../catalog-item/catalog-item.component';
+import './catalog-menu.styles.scss';
 
 const CatalogMenu = () => {
   // State variables
@@ -15,10 +16,10 @@ const CatalogMenu = () => {
   }
 
   return (
-    <div className='catalog-container'>
-      <SearchBox onChangeHandler={onSearchChange}/>
+    <div className='content-container'>
+      <SearchBox onChangeHandler={onSearchChange} className='content-search-box' />
       {/* Getting the data from CatalogData and Maps through the array of catalog items */}
-      <div className='catalog-body-container'>
+      <div className='content-body-container'>
         {/* 
           getting the title property using filter and checks if the value of search bar is similar to the "title" propery in the data and also used the map function to iterate the array of data and get data items.
         */}
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './search-box.styles.scss';
 
-const SearchBox = ({onChangeHandler}) => {
+const SearchBox = ({onChangeHandler, className}) => {
   return (
     <div className='searchbox-container'>
       <input
         type='text'
-        className='search-box'
+        className={`search-box ${className}`}
         placeholder='Search'
         onChange={onChangeHandler}
       />
