e377fc39c32d6ad260ac15c6426b00cbec9463e4
Joseph <ralphlsulit@gmail.com>
2022-09-11 07:31:42 +0800

    change search inut design navbar

--- src/routes/navigation.component.jsx
+++ src/routes/navigation.component.jsx
@@ -1,47 +1,37 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faList, faBell, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import './navigation.styles.scss';
 
 function NavScrollExample() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className='navbar-container mb-5' expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand href="#" className='navbar-brand'>Centralized Catalog</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
+        <Navbar.Collapse id="navbarScroll" className='justify-content-end'>
           <Form className="d-flex">
             <Form.Control
               type="search"
               placeholder="Search"
-              className="me-2"
+              className="me-4 shadow-none navbar-search-box no-border"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
           </Form>
+          <Nav
+            className="my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Tools</Nav.Link>
+            <Nav.Link href="#action2">Catalogs</Nav.Link>
+            <Nav.Link href="#action2"><FontAwesomeIcon icon={faList} /></Nav.Link>
+            <Nav.Link href="#action2"><FontAwesomeIcon icon={faBell} /></Nav.Link>
+            <Nav.Link href="#action2"><FontAwesomeIcon icon={faRightFromBracket} /></Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
