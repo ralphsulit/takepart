869cf378525e544acc4d2164f25927d796adab0d
Joseph <ralphlsulit@gmail.com>
2022-09-11 04:29:22 +0800

    added mobile design

--- src/components/catalog-item/catalog-item.component.jsx
+++ src/components/catalog-item/catalog-item.component.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import './catalog-item.styles.scss';
 
 const CatalogItem = ({ catalog }) => {
   // destructured catalog objects
   const { title, description, number } = catalog;
   return (
-    <div className='catalog-item'>
+    <div className='catalog-item-container'>
       <h3>{title}</h3>
-      <p>{description}</p>
-      <p>Manufucturer M12312 PartNumber:
-        <span className='munufucturer-number'>{number}</span>
-      </p>
-      <p className='catalog-count'>Found in your catalog and 5 others</p>
+      <div className='catalog-item-group'>
+        <p className='catalog-description'>{description}</p>
+        <p>Manufucturer M12312 PartNumber:
+          <span className='catalog-number'>{number}</span>
+        </p>
+        <p className='catalog-count'>Found in your catalog and 5 others</p>
+      </div>
     </div>  
   );
 };
